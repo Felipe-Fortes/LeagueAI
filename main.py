b2fe805f112f8b1b96ca3561e300e48f9401875a
Felipe <91634101+Felipe-Fortes@users.noreply.github.com>
2025-06-12 12:00:31 -0300

    PIPIPIPOPOPO

--- main.py
+++ main.py
@@ -12,15 +12,14 @@ if not api_key:
 api_url = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/%s/%s" % (gameName, tagLine)
 api_url = api_url + "?api_key=" + api_key
 
-
 request = requests.get(api_url)
+puuid = request.json().get("puuid")
 
 
 # Função pra obter o summoner
 def get_summoner():
     if request.status_code == 200:
         print("Request was successful!")
-        puuid = request.json().get("puuid")
         summoner_url = "https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/%s" % puuid
         summoner_url = summoner_url + "?api_key=" + api_key
         summoner_request = requests.get(summoner_url)
@@ -33,7 +32,6 @@ def get_summoner():
 def get_summoner_matches():
     if request.status_code == 200:
         print("Request was successful!")
-        puuid = request.json().get("puuid")
         match_url = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/%s/ids" % puuid
         match_url = match_url + "?start=0&count=20&api_key=" + api_key
         match_request = requests.get(match_url)
