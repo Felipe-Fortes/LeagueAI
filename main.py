8bd089bbc64d6f82b7ebf1e5c725b66894129df9
Felipe <91634101+Felipe-Fortes@users.noreply.github.com>
2025-06-12 05:58:45 -0300

    Localização de conta pelo puuid

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ api_url = api_url + "?api_key=" + api_key
 request = requests.get(api_url)
 if request.status_code == 200:
     print("Request was successful!")
-    data = request.json()
+    puuid = request.json().get("puuid")
+    summoner_url = "https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/%s" % puuid
+    summoner_url = summoner_url + "?api_key=" + api_key
+    summoner_request = requests.get(summoner_url)
+    data = summoner_request.json()
     print(data)
 else:
     print(f"Request failed with status code: {request.status_code}")
